dda92a8c27962a27de12628713eebe9760e87949
Rick Li <ricklihs@hotmail.com>
2017-11-20 03:32:35 -0800

    add app-routing , home ,page-not-found

---
+++ singleLancer/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path : 'home', component : HomeComponent},
+  { path : '', redirectTo : '/home', pathMatch : 'full'},
+  { path : '**', component : PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule, RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
