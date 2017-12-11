1081e5c621c2df016c0f48855b1a0e03bafb7c64
Rick Li <ricklihs@hotmail.com>
2017-12-10 19:50:30 -0800

    add animations.ts

--- singleLancer/src/app/heroes/hero-detail.component.ts
+++ singleLancer/src/app/heroes/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+
+import {} from '../animations';
 
 @Component({
   selector: 'app-hero-detail',
