b199a5bd21124a68723947300c5cd987b1faa28f
Rick Li <ricklihs@hotmail.com>
2017-12-10 21:28:52 -0800

    add hero-detail.component.ts

--- singleLancer/src/app/heroes/hero-detail.component.ts
+++ singleLancer/src/app/heroes/hero-detail.component.ts
@@ -1,17 +1,39 @@
-// import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Observable} from 'rxjs/Observable';
+import { Router, ActivatedRoute, ParamMap} from '@angular/router';
 
-import {} from '../animations';
+import { slideInDownAnimation} from '../animations';
+
+import { Hero, HeroService} from './hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
+  animations: [ slideInDownAnimation]
 })
 export class HeroDetailComponent implements OnInit {
+  @HostBinding('@routeAnimation') routeAnimation= true;
+  @HostBinding('style.display') display= 'block';
+  @HostBinding('style.position') position= 'absolute';
 
-  constructor() { }
+  hero$: Observable<Hero>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: HeroService
+  ) { }
 
   ngOnInit() {
+    this.hero$ = this.route.paramMap
+      .switchMap((params: ParamMap) =>
+        this.service.getHero(params.get('id')));
   }
 
+  gotoHeroes(hero: Hero) {
+    const heroId = hero ? hero.id : null;
+    this.router.navigate(['/heroes', {id: heroId, foo: 'foo'}]);
+  }
 }
