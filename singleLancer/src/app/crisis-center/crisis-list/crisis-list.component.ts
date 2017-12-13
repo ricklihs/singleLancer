477b6cc431edaf180a84fb2a54683f83a7a79408
Rick Li <ricklihs@hotmail.com>
2017-12-13 01:26:50 -0800

    Modify crisis-list.component.ts html

--- singleLancer/src/app/crisis-center/crisis-list/crisis-list.component.ts
+++ singleLancer/src/app/crisis-center/crisis-list/crisis-list.component.ts
@@ -1,4 +1,9 @@
+import 'rxjs/add/operator/switchMap';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { Crisis, CrisisService} from '../crisis.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-crisis-list',
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crisis-list.component.css']
 })
 export class CrisisListComponent implements OnInit {
+  crises$: Observable<Crisis[]>;
+  selectedId: number;
 
-  constructor() { }
+  constructor(
+    private service: CrisisService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.crises$ = this.route.paramMap
+      .switchMap((params: ParamMap) => {
+        this.selectedId = +params.get('id');
+        return this.service.getCrises();
+      });
   }
-
 }
