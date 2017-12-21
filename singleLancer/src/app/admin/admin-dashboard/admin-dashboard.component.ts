af06b978e6aa7cfbc45c8afe118ee315fc3b0e25
Rick Li <ricklihs@hotmail.com>
2017-12-21 02:42:22 -0800

    modify admin-dashboard.component.ts

--- singleLancer/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ singleLancer/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+// import { SelectivePreloadingStrategy } from '../selective-preloading-strategy';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,10 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+  sessionId: Observable<string>;
+  token: Observable<string>;
+  modules: string[];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    // private preloadStrategy: SelectivePreloadingStrategy
+  ) {
+    // this.modules=preloadStrategy.preloadedModules;
+  }
 
   ngOnInit() {
+    // Capture the session ID if available
+    this.sessionId = this.route
+      .queryParamMap
+      .map(params => params.get('session_id') || 'None');
+
+    // Capture the fragment if available
+    this.token = this.route
+      .fragment
+      .map( fragment => fragment || 'None');
   }
 
 }
