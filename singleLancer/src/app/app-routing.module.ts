6bb6485a376eb9c7d04589013ff4160ef8a6b837
Rick Li <ricklihs@hotmail.com>
2017-12-21 11:43:41 -0800

    add app.routing.ts

--- singleLancer/src/app/app-routing.module.ts
+++ singleLancer/src/app/app-routing.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules} from '@angular/router';
 import { HomeComponent} from './home/home.component';
 
-
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 import { PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-
-
+import { CanDeactivateGuard } from './can-deactivate-guard.service';
+import { AuthGuard } from './auth-guard.service';
+import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
 
 const appRoutes: Routes = [
-  { path : 'home', component : HomeComponent},
-
+  { path : 'home', component : HomeComponent },
+  {
+    path : 'compose',
+    component: ComposeMessageComponent,
+    outlet: 'popup'
+  },
+  {
+    path: 'admin',
+    loadChildren: 'app/admin/admin.module#AdminModule',
+    canLoad: [AuthGuard]
+  },
   {
     path: 'crisis-center1',
     loadChildren: 'app/crisis-center1/crisis-center.module#CrisisCenterModule',
@@ -35,9 +45,18 @@ const appRoutes: Routes = [
     // CommonModule,
     RouterModule.forRoot(
       appRoutes,
-      {enableTracing: true})
+      {
+        enableTracing: true,
+        preloadingStrategy: SelectivePreloadingStrategy
+      })
       // {enableTracing: true, preloadingStrategy: PreloadAllModules})
   ],
-  exports: [RouterModule]
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    CanDeactivateGuard,
+    SelectivePreloadingStrategy
+  ]
 })
 export class AppRoutingModule { }
